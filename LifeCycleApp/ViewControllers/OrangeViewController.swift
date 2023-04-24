6db7465b0c3c1cb112bad36f1e398527223ee889
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-24 20:30:25 +0300

    1. Init added

--- LifeCycleApp/ViewControllers/OrangeViewController.swift
+++ LifeCycleApp/ViewControllers/OrangeViewController.swift
@@ -7,11 +7,40 @@
 
 import UIKit
 
-class OrangeViewController: UIViewController {
+final class OrangeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
     }
     
-}
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    // Вызывается после того, как объекты были десериализированы из файла Interface Builder,
+    // и перед началом жизненного цикла UIViewController
+//    override func awakeFromNib() {
+//        super.viewDidLoad()
+//        printMessage()
+//    }
+    
+    // Вызывается для создания представления, если оно еще не было создано.
+//    override func loadView() {
+//        // только при программной в верстке, для ручного создания view
+//    }
+    
+//
+//    // Вызывается после загрузки View в память
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        title = "GreenVC"
+//        printMessage()
+//    }
+//
+//}
+//
+//extension UIViewController {
+//    func printMessage(function: String = #function) {
+//        print("\(title ?? ""): \(function)")
+//    }
+//}
