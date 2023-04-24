78cfe46a45e7dcc5cd02edec6be13ecb2efbad7e
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-24 20:16:28 +0300

    Classes GreenVC and OrangeVC added

---
+++ LifeCycleApp/ViewControllers/GreenViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LifeCycleApp
+//
+//  Created by Vsevolod Lashin on 24.04.2023.
+//
+
+import UIKit
+
+final class GreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
--- LifeCycleApp/ViewControllers/OrangeViewController.swift
+++ LifeCycleApp/ViewControllers/OrangeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  OrangeViewController.swift
 //  LifeCycleApp
 //
 //  Created by Vsevolod Lashin on 24.04.2023.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OrangeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
-
-
+    
 }
-
